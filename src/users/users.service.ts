ae7e43d6f943506a6d9cd604d53bd9a88118d07a
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-02-25 09:32:32 +0700

    Edit : updateUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { validate } from 'class-validator';
+import { Role } from 'src/role/entities/role.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @InjectRepository(Role)
+    private readonly rolesRepository: Repository<Role>,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -19,6 +22,13 @@ export class UsersService {
       throw new Error(`Validation failed: ${errors.toString()}`);
     }
     const newUser = this.usersRepository.create(createUserDto);
+    if (createUserDto.roleIds) {
+      const roles = await this.rolesRepository.find({
+        where: { id: In(createUserDto.roleIds) },
+      });
+      newUser.roles = roles;
+    }
+
     return await this.usersRepository.save(newUser);
   }
 
@@ -27,20 +37,29 @@ export class UsersService {
   }
 
   async findOne(id: number): Promise<User> {
-    const user = await this.usersRepository.findOneBy({ id });
-    if (!user) {
-      throw new NotFoundException(`User with ID ${id} not found`);
-    }
+    const user = await this.usersRepository.findOneOrFail({
+      where: { id },
+      relations: ['roles'],
+    });
+
     return user;
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
-    await this.usersRepository.update(id, updateUserDto);
-    const updatedUser = await this.usersRepository.findOneBy({ id });
-    if (!updatedUser) {
-      throw new NotFoundException(`User with ID ${id} not found`);
+    const user = await this.usersRepository.findOneOrFail({
+      where: { id },
+      relations: ['roles'],
+    });
+    user.roles = []; // clear roles before update
+    await this.usersRepository.save(user);
+    if (updateUserDto.roleIds) {
+      const roles = await this.rolesRepository.find({
+        where: { id: In(updateUserDto.roleIds) },
+      });
+      user.roles = roles; //define roles ใหม่
     }
-    return updatedUser;
+    const updateUser = { ...user, ...updateUserDto };
+    return await this.usersRepository.save(updateUser);
   }
 
   async remove(id: number): Promise<void> {
